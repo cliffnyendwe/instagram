955eacbdeefa83da82edad157f1b5d5b9394b31e
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-18 19:13:18 +0300

    added project name to procfile

--- instagram/settings.py
+++ instagram/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
        )
-   }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
